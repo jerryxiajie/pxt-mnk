a92fb167c03806bf44f80150edf00c022da0770f
Jerry Xia <jerry.xia@embed.com>
2017-11-21 10:25:58 +0800

    New test

--- libs/MiNodeMIC.cpp
+++ libs/MiNodeMIC.cpp
@@ -15,17 +15,53 @@ MiNodeMIC::~MiNodeMIC()
   system_timer_remove_component(this);
 }
 
-void MiNodeMIC::update(void)
+void MiNodeMIC::checking(void)
 {
-  static int adHolder = 530;
-  int newValue = pin->read_u16();
+  static int adHolder[8] = {0,0,0,0,0,0,0,0};
+  static int ad_count = 0;
+  int temp = 0;
+  int i=0;
+  int current_ad = 0;
+  static int tri_flag = 0;
+  static int time_count = 0;
 
-  if(newValue - adHolder > 20)
+  current_ad = pin->read_u16();
+  if (current_ad > 528)
   {
-    MicroBitEvent evt(this->baseId + this->id,MINODE_MIC_EVT_NOISE);
+    adHolder[ad_count%8] = current_ad - 528;
+    ad_count++;
   }
-  adHolder = newValue;
+  else
+  {
+    adHolder[ad_count%8] = 528 - current_ad;
+    ad_count++;
+  }
 
+  if (tri_flag == 1)
+  {
+    time_count++;
+    if (time_count > 8000)
+    {
+      tri_flag = 0;
+      time_count = 0;
+    }
+  }
+
+  if(tri_flag == 0)
+  {
+    for(i=0; i<8; i++)
+    {
+      temp += adHolder[i];
+    }
+    temp /= 8;
+
+    if(temp > 400)
+    {
+      MicroBitEvent evt(this->baseId + this->id,MINODE_MIC_EVT_NOISE);
+      tri_flag = 1;
+      temp = 0;
+    }
+  }
 }
 
 void MiNodeMIC::attach(AnalogConnName connName)
@@ -42,54 +78,12 @@ void MiNodeMIC::attach(AnalogConnName connName)
   }
   pin = new AnalogIn(pinName);
 
-  NRF_ADC->CONFIG = (ADC_CONFIG_RES_8bit << ADC_CONFIG_RES_Pos) |
-                    (ADC_CONFIG_INPSEL_AnalogInputOneThirdPrescaling << ADC_CONFIG_INPSEL_Pos) |
-                    (ADC_CONFIG_REFSEL_SupplyOneThirdPrescaling << ADC_CONFIG_REFSEL_Pos) |
-                    (pinName << ADC_CONFIG_PSEL_Pos) |
-                    (ADC_CONFIG_EXTREFSEL_None << ADC_CONFIG_EXTREFSEL_Pos);
-
   // ToDo: Init a timer      
-  timer.attach_us(this, &MiNodeMIC::update, 50);
+  timer.attach_us(this, &MiNodeMIC::checking, 500);
 }
 
 
 int MiNodeMIC::getMicLevel()
 {
-  unsigned int temp=0;
-  temp = pin->read_u16();
-  if (temp > 530)
-  {
-    temp -= 530;
-  }
-  else
-  {
-    temp = 530-temp;
-  }
-
-  if ((temp > MICROBIT_MIC_MIN) && (temp < MICROBIT_MIC_LEVEL_A+1))
-  {
-    return MICROBIT_MIC_LEVEL_1;
-  }
-  else if ((temp > MICROBIT_MIC_LEVEL_A) && (temp < MICROBIT_MIC_LEVEL_B+1))
-  {
-    return MICROBIT_MIC_LEVEL_2;
-  }
-  else if ((temp > MICROBIT_MIC_LEVEL_B) && (temp < MICROBIT_MIC_LEVEL_C+1))
-  {
-    return MICROBIT_MIC_LEVEL_3;
-  }
-  else if ((temp > MICROBIT_MIC_LEVEL_C) && (temp < MICROBIT_MIC_LEVEL_D+1))
-  {
-    return MICROBIT_MIC_LEVEL_4;
-  }
-  else if ((temp > MICROBIT_MIC_LEVEL_D) && (temp < MICROBIT_MIC_MAX+1))
-  {
-    return MICROBIT_MIC_LEVEL_5;
-  }
-  else
-  {
-    return MICROBIT_MIC_LEVEL_ERROR;
-  }
+  timer.detach();
 }
-
-
